67ad7a2dc83569e983cb3054216aae0dabcf0d7a
RequireSun <kelvinsen@outlook.com>
2020-11-29 12:39:36 +0800

    feat: init ts proj

---
+++ src/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export interface Props {
+    label?: string;
+    type?: 'string' | 'number';
+    value: string | number;
+    required?: boolean;
+    onChange: (val: string | number) => void;
+}
+
+export default class MyInput extends React.Component<Props> {
+    static defaultProps = {
+        type: 'string',
+    };
+
+    onChange = (e) => {
+        const {
+            type,
+            onChange,
+        } = this.props;
+
+        onChange(type === 'string' ? e.target.value : +e.target.value);
+    };
+
+    render() {
+        const {
+            label,
+            value,
+        } = this.props;
+
+        return (
+            <div>
+                {label ? (<label>{label}</label>) : null}
+                <input type="text" value={value}
+                       onChange={this.onChange}/>
+            </div>
+        );
+    }
+}
