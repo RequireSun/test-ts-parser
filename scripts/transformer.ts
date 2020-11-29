03d756a4b619bbf179de005a95bbf53e8c497a36
RequireSun <kelvinsen@outlook.com>
2020-11-29 15:13:14 +0800

    feat: 继续推进

--- scripts/transformer.ts
+++ scripts/transformer.ts
@@ -4,7 +4,16 @@ import TS from 'typescript';
 
 export default function (program: TS.Program): TS.TransformerFactory<TS.SourceFile> {
     return (context: TS.TransformationContext) => (file: TS.SourceFile) => {
-        console.log(file);
-        return file;
+        // console.log(file);
+        return visitNodeAndChildren(file, program, context);
     };
+}
+
+function visitNodeAndChildren(node, program, context) {
+    return TS.visitEachChild(visitNode(node, program), childNode => visitNodeAndChildren(childNode, program, context), context);
+}
+
+function visitNode(node, program) {
+    console.log(node);
+    return node;
 }
